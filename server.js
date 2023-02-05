638bba1509e641116bf6c3a73795fa739bfb0302
jack pillare <aarryann@yahoo.com>
2023-02-04 21:23:53 -0500

    remote call fix...

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/run-commands', (req, res) => {
   });
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 
