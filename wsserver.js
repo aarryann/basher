ecae7f5c664628b3e5d6444f76c07d631944026c
Jack Pillai <aarryann@yahoo.com>
2023-02-07 21:00:04 -0500

    wsclient

--- wsserver.js
+++ wsserver.js
@@ -4,6 +4,14 @@ const server = require('http').createServer(app);
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ server });
 const { spawn } = require('child_process');
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
