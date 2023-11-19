6355c0b219f6da838f75e60d83a386b461e04e08
Vivek Pillai <aarryann@yahoo.com>
2023-11-18 19:22:41 -0600

    updates...

--- public/components/header.js
+++ public/components/header.js
@@ -8,16 +8,18 @@ if (typeof window !== 'undefined') {
 
   // eslint-disable-next-line
   contentTemplate.innerHTML = html`
-  <h1 class="text-3xl font-bold text-center" x-text="sitename"></h1>
-  <div class="container pt-4 mx-auto">
-    <div>
-      <a class="mr-2 py-2 px-4 shadow text-center hover:bg-indigo-100 hover:shadow-lg hover:rounded transition duration-150 ease-in-out transform hover:scale-105" href="/dash">Dash</a>
-      <a class="m-2 py-2 px-4 shadow text-center hover:bg-indigo-100 hover:shadow-lg hover:rounded transition duration-150 ease-in-out transform hover:scale-105" href="/commands">Edit commands</a>
-      <a class="m-2 py-2 px-4 shadow text-center hover:bg-indigo-100 hover:shadow-lg hover:rounded transition duration-150 ease-in-out transform hover:scale-105" href="/settings">Settings</a>
-      <a class="m-2 py-2 px-4 shadow text-center hover:bg-indigo-100 hover:shadow-lg hover:rounded transition duration-150 ease-in-out transform hover:scale-105" href="/logs">View Logs</a>
-      <a class="m-2 py-2 px-4 shadow text-center hover:bg-indigo-100 hover:shadow-lg hover:rounded transition duration-150 ease-in-out transform hover:scale-105" href="/queue">Job queues</a>
+  <header class="fixed bg-indigo-600 p-4 text-white text-center">
+    <h1 class="text-3xl font-bold text-center" x-text="sitename"></h1>
+    <div class="container pt-4 mx-auto">
+      <div>
+        <a class="mr-2 py-2 px-4 shadow text-center hover:bg-indigo-100 hover:shadow-lg hover:rounded transition duration-150 ease-in-out transform hover:scale-105" href="/dash">Dash</a>
+        <a class="m-2 py-2 px-4 shadow text-center hover:bg-indigo-100 hover:shadow-lg hover:rounded transition duration-150 ease-in-out transform hover:scale-105" href="/commands">Edit commands</a>
+        <a class="m-2 py-2 px-4 shadow text-center hover:bg-indigo-100 hover:shadow-lg hover:rounded transition duration-150 ease-in-out transform hover:scale-105" href="/settings">Settings</a>
+        <a class="m-2 py-2 px-4 shadow text-center hover:bg-indigo-100 hover:shadow-lg hover:rounded transition duration-150 ease-in-out transform hover:scale-105" href="/logs">View Logs</a>
+        <a class="m-2 py-2 px-4 shadow text-center hover:bg-indigo-100 hover:shadow-lg hover:rounded transition duration-150 ease-in-out transform hover:scale-105" href="/queue">Job queues</a>
+      </div>
     </div>
-  </div>
+  </header>
   `;
 
   class SiteTopNav extends HTMLElement {
@@ -30,4 +32,7 @@ if (typeof window !== 'undefined') {
       this.appendChild(node);
     }
   }
+
+  customElements.define('site-top-nav', SiteTopNav);
+
 }
