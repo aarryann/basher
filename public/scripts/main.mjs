35c4a2f2de78516bf90b0e622a3ea8cd26fa8203
Vivek Pillai <aarryann@yahoo.com>
2024-09-07 14:22:35 -0400

    modular

--- public/scripts/main.mjs
+++ public/scripts/main.mjs
@@ -154,3 +154,121 @@ export const dashboard = () => ({
     }
   }
 })
+
+export const card = () => ({
+  expandedWidget: null,
+  holdTimer: null,
+  holdTriggered: false,
+  touchHandled: false,
+  countdownTimer: null,
+  widgetClass: null,
+  iconClass: null,
+  downCount: null,
+  title: null,
+  icon: "",
+  index: null,
+  logs: null,
+
+  async init() {
+    let widget = this.$data.widget;
+    this.index = this.$data.index;
+    this.iconClass = `fas fa-${widget.icon} mb-1`;
+    this.downCount = null;
+    this.widgetClass = `col-span-${widget.colspan}`;
+    this.title = widget.title;
+    this.icon = widget.icon;
+  },
+
+  isMessageDisplayHidden() {
+    return !(this.displayMessage && this.displayMessage.length > 0);
+  },
+
+  getCountdownClass() {
+    return this.downCount >= 0 ? 'fade-out' : '';
+  },
+
+  startHold(event) {
+    this.displayMessage = null;
+    //const index = event.currentTarget.getAttribute('data-index');
+    const isTouch = event.type.startsWith('touch');
+
+    if (isTouch) {
+      this.touchHandled = true;
+    } else if (this.touchHandled) {
+      // This is to avoid multiple clicks (touch and mouse) when mouseclicked in google mobile device mode. Here the mouseclick after touch is ignored
+      return;
+    }
+    this.holdTimer = setTimeout(() => {
+      this.showLogs(this.index);
+      this.holdTriggered = true;
+    }, 1000);
+  },
+
+  endHold(event) {
+    //const index = event.currentTarget.getAttribute('data-index');
+    const isTouch = event.type.startsWith('touch');
+
+    if (isTouch) {
+      // No purpose for this code as no known problems. This is to avoid touchHandled remaning true in touch mode
+      setTimeout(() => { this.touchHandled = false; }, 10);
+    }
+    else if (this.touchHandled) {
+      // This is to avoid multiple clicks (touch and mouse) when mouseclicked in google mobile device mode. Here the mouseclick after touch is ignored
+      this.touchHandled = false;
+      return;
+    }
+
+    if (!this.holdTriggered) this.handleWidgetClick(this.index);
+    clearTimeout(this.holdTimer);
+    this.holdTriggered = false;
+  },
+
+  cancelHold() {
+    clearTimeout(this.holdTimer);
+    this.holdTriggered = false;
+  },
+
+  startCountdown() {
+    if (this.countdownTimer) {
+      //this.displayMessage = 'Cancelled Countdown';
+      clearTimeout(this.countdownTimer);
+      this.countdownTimer = null;
+      this.downCount = null;
+    } else {
+      //this.displayMessage = 'Start Countdown';
+      this.countdown(5);
+    }
+  },
+
+  countdown(count) {
+    if (count <= 0) {
+      this.countdownTimer = null;
+      this.handleOnClick(index);
+      this.downCount = null;
+      return;
+    }
+    this.downCount = count;
+    this.countdownTimer = setTimeout(() => {
+      this.downCount = null; // Reset to hide countdown before next number
+      setTimeout(() => { this.countdown(count - 1) }, 200);
+    }, 800);
+  },
+
+  handleWidgetClick() {
+    this.startCountdown();
+  },
+
+  handleOnClick() {
+    console.log('On clicked:', this.title);
+    this.displayMessage = `${this.title} clicked`;
+  },
+
+  showLogs() {
+    if (this.logs) {
+      this.modalContent = this.logs;
+      this.modalVisible = true;
+    } else {
+      console.log('No logs available for:', this.title);
+    }
+  },
+})
