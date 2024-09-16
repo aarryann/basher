d4f131d77daeec5012e2395959724daa86be73f4
Vivek Pillai <aarryann@yahoo.com>
2024-09-16 07:41:49 -0400

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  publicDir: 'static',
+  publicDir: 'assets',
   outDir: './public',
   build: {
     // Ensure Astro doesn't try to handle server-side routing
